1b8e6d2def29c0fd44a95fd33efce8e22241c8f4
math974 <mathias.ballot@epitech.com>
2021-11-17 15:03:15 +0400

    change 'copy array' invalif malloc allocation

--- lib/my/my_function/copy_array.c
+++ lib/my/my_function/copy_array.c
@@ -14,7 +14,7 @@ char **copy_array(char **src_array)
     char **new_array = malloc(sizeof(char *) * (len_array(src_array) + 1));
 
     for (int i = 0; src_array[i] != NULL; i++) {
-        new_array[i] = malloc(sizeof(char) * my_strlen(src_array[i]) + 1);
+        new_array[i] = malloc(sizeof(char) * (my_strlen(src_array[i]) + 1));
         for (int i_str = 0; src_array[i][i_str] != '\0'; i_str++)
             new_array[i][i_str] = src_array[i][i_str];
     }
