37b1b2804dc4a2b5634afb0839431273e4bcc4d3
math974 <mathias.ballot@epitech.com>
2021-12-23 21:42:03 +0400

    change the documentation of my_stdlib for the 80 lignes

--- lib/my/include/my_stdlib.h
+++ lib/my/include/my_stdlib.h
@@ -17,12 +17,16 @@
 //    |_| |_| |_|\__, |___|___/\__\__,_|_|_|_.__/
 //               |___/_____|
 //
-//    The library <my_stdlib.h> (for MY standard library) offers functions to answer different problems.
+//    The library <my_stdlib.h> (for MY standard library) offers
+//    functions to answer different problems.
 //    First of all, this library will allow you to manage memory dynamically,
 //    by allocating or freeing memory in the heap.
-//    It will also allow you to interact with the operating system on which the program runs.
-//    This library also offers a number of other utility functions for conversions between data types,
-//    algorithms (sorting, dichotomous search, pseudo-random number generation, ...)
+//    It will also allow you to interact with the operating system
+//    on which the program runs.
+//    This library also offers a number of other utility functions
+//    for conversions between data types,
+//    algorithms (sorting, dichotomous search,
+//    pseudo-random number generation, ...)
 //    and some other possibilities.
 //
 //
@@ -36,7 +40,8 @@ typedef unsigned long my_size_t;
 #define ABS(x)  (x > 0) ? (x) : (-x)
 
 ////////////////////////////////////////////////////////////
-/// \brief If all goes well, the function returns a pointer to the newly allocated area.
+/// \brief If all goes well, the function returns a pointer to the
+/// newly allocated area.
 /// It is notably used by the div function.
 ///
 /// \param quot the quotient
@@ -104,7 +109,8 @@ int my_abs(int nbr);
 ///
 /// \param nbr the value for which the absolute value must be calculated.
 ///
-/// \return returns the absolute value of the long integer proposed in parameter.
+/// \return returns the absolute value of the long integer
+/// proposed in parameter.
 ///
 ////////////////////////////////////////////////////////////
 long my_labs(long nbr);
@@ -117,7 +123,8 @@ long my_labs(long nbr);
 ///
 /// \param nbr the value for which the absolute value must be calculated.
 ///
-/// \return returns the absolute value of the very long intege proposed in parameter.
+/// \return returns the absolute value of the very long integer
+/// proposed in parameter.
 ///
 ////////////////////////////////////////////////////////////
 long long my_llabs(long long nbr);
@@ -158,10 +165,13 @@ my_ldiv_t my_ldiv(long dividend, long divisor);
 /// This function allows to transform a string of characters,
 /// representing an integer value, into a numerical value of type int.
 ///
-/// \param str this parameter contains the string of characters to be converted into a data of type int.
+/// \param str this parameter contains the string of characters to be
+/// converted into a data of type int.
 ///
-/// \return If the string to be converted does not contain an integer numeric data, the value 0 will be returned.
-/// Otherwise, a value of type int equivalent to the string passed as parameter will be returned.
+/// \return If the string to be converted does not contain
+/// an integer numeric data, the value 0 will be returned.
+/// Otherwise, a value of type int equivalent to the string
+/// passed as parameter will be returned.
 ///
 ////////////////////////////////////////////////////////////
 int my_atoi(const char *str);
@@ -173,7 +183,8 @@ int my_atoi(const char *str);
 /// This function transforms a string, representing a long integer value,
 /// into a long numeric value.
 ///
-/// \param str This parameter contains the string to be converted to a long data type.
+/// \param str This parameter contains the string to be converted
+/// to a long data type.
 ///
 /// \return If the string to be converted does not contain an integer data,
 /// the value 0 will be returned. Otherwise, a value of type long equivalent
@@ -190,9 +201,11 @@ long my_atol(const char *str);
 /// representation of an integer, into a very long integer
 /// (type long long or long long int: these are synonyms).
 ///
-/// \param str This parameter contains the string to be converted into a long long (or long long int) data type.
+/// \param str This parameter contains the string to be converted
+/// into a long long (or long long int) data type.
 ///
-/// \return If the string to be converted does not contain an integer data, the value 0 will be returned.
+/// \return If the string to be converted does not contain an integer data,
+/// the value 0 will be returned.
 /// Otherwise, a value of type long long (or long long int)
 /// equivalent to the string passed in parameter will be returned.
 ///
@@ -201,14 +214,16 @@ long long my_atoll(const char *str);
 
 
 ////////////////////////////////////////////////////////////
-/// \brief This function transforms a character string, representing a floating value,
-/// into a numerical value of type double
+/// \brief This function transforms a character string,
+/// representing a floating value, into a numerical value of type double
 ///
-/// \param str This parameter contains the string to be converted to a double data type.
+/// \param str This parameter contains the string to be converted
+/// to a double data type.
 ///
-/// \return If the string to be converted does not contain a floating-point numeric data,
-/// the value 0 will be returned. Otherwise, a value of type double equivalent to the string
-/// passed in parameter will be returned.
+/// \return If the string to be converted does not contain
+/// a floating-point numeric data,
+/// the value 0 will be returned. Otherwise, a value of type double equivalent
+/// to the string passed in parameter will be returned.
 ///
 ////////////////////////////////////////////////////////////
 double my_atof(const char *str);
@@ -218,23 +233,33 @@ double my_atof(const char *str);
 /// \brief reallocate a block of memory
 ///
 /// This function is used to reallocate a block of memory in the heap.
-/// This means that if the free memory space following the block to be reallocated is large enough,
-/// the memory block is simply enlarged. On the other hand, if the free space is not sufficient,
-/// a new block of memory will be allocated, the contents of the original area will be copied
-/// into the new area and the original block of memory will be freed automatically.
+/// This means that if the free memory space following the block
+/// to be reallocated is large enough, the memory block is simply enlarged.
+/// On the other hand, if the free space is not sufficient,
+/// a new block of memory will be allocated,
+/// the contents of the original area will be copied
+/// into the new area and the original block of memory
+/// will be freed automatically.
 ///
 /// \param ptr the memory address of the memory block to be reallocated.
 ///
 /// \param old_size old size (in bytes) to the old memory block.
 ///
-/// \param mem_size allows you to specify the size (in bytes) of the memory block to be allocated.
+/// \param mem_size allows you to specify the size (in bytes)
+/// of the memory block to be allocated.
 ///
-/// \return If all goes well, the function returns a pointer to the reallocated memory area.
-/// Be careful, this pointer is a generic pointer (void *): if necessary, it is your responsibility
-/// to "cast" your pointer to another type (at least if you don't want to have a warning displayed
-/// by the compiler, which I strongly recommend). In the event that the memory block cannot be reallocated
-/// to you (no more memory available, for example), the NULL pointer will be returned.
-/// One should normally and systematically test that the function does not return this value.
+/// \return If all goes well, the function returns a pointer
+/// to the reallocated memory area.
+/// Be careful, this pointer is a generic pointer (void *): if necessary,
+/// it is your responsibility
+/// to "cast" your pointer to another type
+/// (at least if you don't want to have a warning displayed
+/// by the compiler, which I strongly recommend).
+/// In the event that the memory block cannot be reallocated
+/// to you (no more memory available, for example),
+/// the NULL pointer will be returned.
+/// One should normally and systematically
+/// test that the function does not return this value.
 /// However, this is not always done by developers.
 ///
 ////////////////////////////////////////////////////////////
@@ -242,16 +267,21 @@ void *my_realloc(void *ptr, my_size_t old_size, my_size_t mem_size);
 
 
 ////////////////////////////////////////////////////////////
-/// \brief This function allocates a block of memory by initializing all these bytes to the value 0.
+/// \brief This function allocates a block of memory
+/// by initializing all these bytes to the value 0.
 ///
-/// \param count allows you to specify the number of consecutive elements to reserve in the memory block.
+/// \param count allows you to specify the number of consecutive elements
+/// to reserve in the memory block.
 ///
 /// \param size allows you to set the size (in number of bytes) of an element.
 ///
-/// \return If all goes well, the function returns a pointer to the newly allocated area.
+/// \return If all goes well, the function returns a pointer
+/// to the newly allocated area.
 /// Be careful, this pointer is of the generic pointer type (void *):
-/// if necessary, it is your responsibility to "cast" your pointer to another type
-/// (at least if you do not want to have a warning displayed by the compiler, which I strongly recommend).
+/// if necessary, it is your responsibility to "cast"
+/// your pointer to another type
+/// (at least if you do not want to have a warning displayed by the compiler,
+/// which I strongly recommend).
 ///
 ////////////////////////////////////////////////////////////
 void *my_calloc(my_size_t count, my_size_t size);
